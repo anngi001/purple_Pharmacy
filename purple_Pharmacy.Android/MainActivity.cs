3bb1df82a0cc8d9bb61149ac519b538c40a0179f
JuanVanegas <jcvanegas@unac.edu.co>
2019-10-30 16:02:41 -0500

    Splash
    Splash del inicio

--- purple_Pharmacy.Android/MainActivity.cs
+++ purple_Pharmacy.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace purple_Pharmacy.Droid
 {
-    [Activity(Label = "purple_Pharmacy", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "purple_Pharmacy", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
